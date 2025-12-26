19360768d05be63410244df2afc6e5207d78bae9
bala-amypo <bala@amypo.in>
2025-12-26 06:42:29 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/ZoneServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/ZoneServiceImpl.java
@@ -3,6 +3,7 @@ package com.example.demo.service.impl;
 import com.example.demo.entity.Zone;
 import com.example.demo.exception.BadRequestException;
 import com.example.demo.exception.ResourceNotFoundException;
+import org.springframework.stereotype.Service;
 import com.example.demo.repository.ZoneRepository;
 import com.example.demo.service.ZoneService;
 
