899e41f3d1342705fc78ce1266bbca4ede901ccd
bala-amypo <bala@amypo.in>
2025-12-26 18:26:23 +0000

    User commit

---
+++ src/main/java/com/example/demo/dto/AuthRequest.java
@@ -0,0 +1,35 @@
+package com.example.demo.dto;
+
+public class AuthRequest {
+
+    private Long userId;
+    private String email;
+    private String role;
+
+    public AuthRequest() {
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
---
+++ src/main/java/com/example/demo/dto/ZoneDTO.java
@@ -0,0 +1,53 @@
+package com.example.demo.dto;
+
+public class ZoneDTO {
+
+    private Long id;
+    private String zoneName;
+    private Integer priorityLevel;
+    private Integer population;
+    private Boolean active;
+
+    public ZoneDTO() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getZoneName() {
+        return zoneName;
+    }
+
+    public void setZoneName(String zoneName) {
+        this.zoneName = zoneName;
+    }
+
+    public Integer getPriorityLevel() {
+        return priorityLevel;
+    }
+
+    public void setPriorityLevel(Integer priorityLevel) {
+        this.priorityLevel = priorityLevel;
+    }
+
+    public Integer getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(Integer population) {
+        this.population = population;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+}
