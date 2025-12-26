58cbb37127fe62b71448ba930bf9148d2e44c33e
bala-amypo <bala@amypo.in>
2025-12-26 17:26:22 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/LoadSheddingService.java
@@ -0,0 +1,15 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.LoadSheddingEvent;
+import java.util.List;
+
+public interface LoadSheddingService {
+
+    LoadSheddingEvent triggerLoadShedding(Long forecastId);
+
+    LoadSheddingEvent getEventById(Long id);
+
+    List<LoadSheddingEvent> getAllEvents();
+
+    List<LoadSheddingEvent> getEventsForZone(Long zoneId);
+}
